53f9ca8e32dbf17b41f1a7c8cd1f73501893fddb
Gabriel Cardoso de Castro <gabrielcdc13@gmail.com>
2024-07-28 18:40:17 -0300

    requisicao cadastro

--- src/telas/Login/Login.js
+++ src/telas/Login/Login.js
@@ -1,20 +1,16 @@
 document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Impede o envio padrão do formulário
+    event.preventDefault();
 
-    // Obtendo valores dos campos
     const email = encodeURIComponent(document.getElementById('email').value);
     const password = encodeURIComponent(document.getElementById('password').value);
 
-    // Validando os campos
     if (!email || !password) {
         alert('Por favor, preencha todos os campos.');
         return;
     }
 
-    // Construindo a URL com os parâmetros
     const url = `https://localhost:44359/api/Auth/sign-in?email=${email}&senha=${password}`;
 
-    // Requisição fetch para o backend
     fetch(url, {
         method: 'POST',
         headers: {
@@ -23,19 +19,17 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     })
     .then(response => {
         if (response.ok) {
-            return response.text(); // Espera-se que a resposta seja o token como string
+            return response.text();
         } else {
             throw new Error('Erro ao fazer login.');
         }
     })
     .then(token => {
-        // Armazena o token no localStorage
         localStorage.setItem('authToken', token);
-        
-        // Redireciona para a tela de menu principal
-        window.location.href = '../Votacoes/Votacoes.html'; // Substitua 'menuPrincipal.html' pela URL correta
+
+        window.location.href = '../Votacoes/Votacoes.html';
     })
     .catch(error => {
-        console.error('Erro:', error.message); // Exibe o erro no console
+        console.error('Erro:', error.message);
     });
 });
