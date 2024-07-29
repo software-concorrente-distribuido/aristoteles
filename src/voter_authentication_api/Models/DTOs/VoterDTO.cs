dd13c8f4e4ebd207cbe059b4b108fab36d9ff95d
LeonardoRibeirOliveira <leonardo25palmeira250@gmail.com>
2024-07-28 23:44:35 -0300

    Distrito ao voter

--- src/voter_authentication_api/Models/DTOs/VoterDTO.cs
+++ src/voter_authentication_api/Models/DTOs/VoterDTO.cs
@@ -6,5 +6,6 @@
         public string Name { get; set; } = string.Empty;
         public string Delegate { get; set; } = string.Empty;
         public byte[]? PasswordHash { get; set; }
+        public int IdDistrict { get; set; }
     }
 }
