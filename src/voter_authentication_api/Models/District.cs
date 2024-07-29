42cd8bf846c1667ea67eab79373050464c4b1333
LeonardoRibeirOliveira <leonardo25palmeira250@gmail.com>
2024-07-28 23:37:08 -0300

    Adição de distrito estático

--- src/voter_authentication_api/Models/District.cs
+++ src/voter_authentication_api/Models/District.cs
@@ -8,4 +8,27 @@
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
     }
+
+    public static class DistrictData
+    {
+        public static List<District> Districts { get; } = new List<District>
+    {
+        new District
+        {
+            Id = 1,
+            Name = "Ditrito A",
+            Description = "Distrito onde estão localizados todos os votantes A",
+            CreatedAt = new DateTime(2020, 1, 1),
+            UpdatedAt = DateTime.UtcNow
+        },
+        new District
+        {
+            Id = 2,
+            Name = "Distrito B",
+            Description = "Distrito onde estão localizados todos os votantes B",
+            CreatedAt = new DateTime(2020, 1, 1),
+            UpdatedAt = DateTime.UtcNow
+        }
+    };
+    }
 }
