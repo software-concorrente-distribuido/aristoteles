b0c80d098cc4fc5150d30f181cf3b4c1840632b8
Gabriel Cardoso de Castro <gabrielcdc13@gmail.com>
2024-07-30 01:03:28 -0300

    Revert "Adicionando logica de distritos"
    This reverts commit ce898c0ab3f3df1daa6b4488bf385ec189d8ff7a.

--- src/telas/Login/Login.js
+++ src/telas/Login/Login.js
@@ -21,44 +21,15 @@ document.getElementById('login-form').addEventListener('submit', function(event)
         if (response.ok) {
             return response.text();
         } else {
-            return response.text().then(errorText => {
-                throw new Error(errorText || 'Erro ao fazer login.');
-            });
+            throw new Error('Erro ao fazer login.');
         }
     })
     .then(token => {
         localStorage.setItem('authToken', token);
+
         window.location.href = '../Votacoes/Votacoes.html';
     })
     .catch(error => {
-        showCustomPopup();
+        console.error('Erro:', error.message);
     });
-
-    function showCustomPopup() {
-        const existingPopup = document.querySelector('.custom-popup');
-        if (existingPopup) {
-            existingPopup.remove();
-        }
-
-        const popup = document.createElement('div');
-        popup.className = 'custom-popup';
-        popup.innerHTML = `
-            <div class="popup-content">
-                <span class="popup-close">&times;</span>
-                <p>Email e/ou senha incorreto(s)</p>
-            </div>
-        `;
-
-        document.body.appendChild(popup);
-
-        popup.querySelector('.popup-close').addEventListener('click', () => {
-            document.body.removeChild(popup);
-        });
-
-        setTimeout(() => {
-            if (document.body.contains(popup)) {
-                document.body.removeChild(popup);
-            }
-        }, 5000);
-    }
 });
