114dc83178ed2e86950903d31f1ebcdf95cdeeb6
LeonardoRibeirOliveira <leonardo25palmeira250@gmail.com>
2024-07-30 19:09:32 -0300

    Erro Http Election service

--- src/voter_authentication_api/Services/ElectionService.cs
+++ src/voter_authentication_api/Services/ElectionService.cs
@@ -8,33 +8,6 @@ namespace VoterAuthenticationAPI.Services
 {
     public class ElectionService
     {
-        private readonly BlockChainConfig _blockChainConfig;
-        private readonly HttpClient _httpClient;
-
-        public ElectionService(HttpClient httpClient, BlockChainConfig blockChainConfig)
-        {
-            _httpClient = httpClient;
-            _blockChainConfig = blockChainConfig;
-        }
-        public async Task createElection(string nome, string descricao)
-        {
-            var requestBody = new
-            {
-                Nome = nome,
-                Descricao = descricao
-            };
-
-            var jsonRequest = JsonSerializer.Serialize(requestBody);
-            var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
-
-            var response = await _httpClient.PostAsync(_blockChainConfig.Url, content);
-
-            if (!response.IsSuccessStatusCode)
-            {
-                throw new Exception($"Error calling blockchain API: {response.ReasonPhrase}");
-            }
-
-            var responseBody = await response.Content.ReadAsStringAsync();
-        }
+        
     }
 }
